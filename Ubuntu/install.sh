abfb2d6303a3df9c5b6b4d69b158af890daa1236
Ethan Riley <ethanmriley@gmail.com>
2017-08-17 16:31:48 -0400

    Modified install.sh to enable insecure websocket connections to https on Firefox

--- Ubuntu/install.sh
+++ Ubuntu/install.sh
@@ -8,4 +8,15 @@ sudo apt-get -y install npm
 sudo ln -s "$(which nodejs)" /usr/local/bin/node
 sudo npm install -g --unsafe-perm node-arduino
 
+firefox -CreateProfile NodeUser -no-remote
+cd $(find ~/.mozilla/firefox/ -maxdepth 1 -name "*NodeUser*" -print)
+echo 'user_pref("network.websocket.allowInsecureFromHTTPS", true);' > prefs.js
+
+cd
+
 sudo adduser $USER dialout
+
+echo "node-arduino &
+firefox -P NodeUser https://ethanmriley.github.io/node-arduino-visualizer" > run.sh
+
+chmod +x run.sh

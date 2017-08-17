3ecbe9efa6c1bb559725ace4a530aad06ad57107
Ethan Riley <ethanmriley@gmail.com>
2017-08-17 16:40:51 -0400

    Modified install.bat to enable insecure websocket connection to https on Firefox

--- Windows/install.bat
+++ Windows/install.bat
@@ -8,3 +8,7 @@ call npm install node-arduino -g
 move %~dps0install_files\run.bat %~dps0
 
 move %~dps0install.bat %~dps0install_files\
+
+firefox -CreateProfile NodeUser -no-remote
+cd /D "%APPDATA%\Mozilla\Firefox\Profiles\*.NodeUser"
+echo user_pref("network.websocket.allowInsecureFromHTTPS", true);>>prefs.js

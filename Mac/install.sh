d4b069409a5a6ab6a9bc1a0471b7621bb40606fe
Ruofan Liu <ruofanliu94@gmail.com>
2017-10-05 11:10:55 -0400

    redirect the new version to RuofanLiu repository

--- Mac/install.sh
+++ Mac/install.sh
@@ -11,17 +11,8 @@ brew upgrade
 brew install node
 brew install npm
 ln -s "$(which nodejs)" /usr/local/bin/node
-npm install -g --unsafe-perm node-arduino
-
-firefox -CreateProfile NodeUser -no-remote
-cd $(find ~/.mozilla/firefox/ -maxdepth 1 -name "*NodeUser*" -print)
-echo 'user_pref("network.websocket.allowInsecureFromHTTPS", true);' > prefs.js
-
-cd
-
-sudo dscl . -create $USER dialout
-
+sudo npm install -g --unsafe-perm node-arduino
 echo "node-arduino &
-firefox -P NodeUser https://ethanmriley.github.io/node-arduino-visualizer" > run.sh
+firefox -P NodeUser https://github.com/RuofanLiu/node-arduino/tree/pH_better" > run.sh
 
-chmod +x run.sh
+
--- Windows/install_files/run.bat
+++ Windows/install_files/run.bat
@@ -1,2 +1,2 @@
 start cmd /C node-arduino
-start "" https://ethanmriley.github.io/node-arduino-visualizer
+start "" https://github.com/RuofanLiu/node-arduino/tree/pH_better
